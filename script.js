6e9685dd2ca772cb0717079a46f0cea553561373
Ektamadaan <ektamadaan45@gmail.com>
2024-01-22 21:48:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+function calculateDOMLevel(elementId) {
+  // Get the target element by id
+  var targetElement = document.getElementById(elementId);
+
+  if (targetElement) {
+    var domLevel = 0;
+
+    // Loop through parent elements until the root html element is reached
+    while (targetElement.parentElement) {
+      targetElement = targetElement.parentElement;
+      domLevel++;
+    }
+
+    // Display the calculated DOM level in an alert dialog
+    alert("The level of the element is: " + domLevel);
+  } 
+}
+
+// Call the function with the id of the target element
+calculateDOMLevel('level');
